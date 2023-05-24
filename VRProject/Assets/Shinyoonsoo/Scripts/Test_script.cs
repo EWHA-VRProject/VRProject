c30e81de54622cd7aba0f2f95823f90fa51b2355
yoonsoodev <yoonsoo98@ewhain.net>
2023-05-24 19:22:27 +0900

    [Movement] 타겟 이동 및 플레이어 터치 시 멈춤 구현

--- VRProject/Assets/Shinyoonsoo/Scripts/Test_script.cs
+++ VRProject/Assets/Shinyoonsoo/Scripts/Test_script.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -9,8 +9,9 @@ public class Test_script : MonoBehaviour
 
 
     NavMeshAgent agent;
-   public Animator ani;
-   public GameObject aim_point;
+    public Animator ani;
+    public GameObject aim_point;
+    [SerializeField] private Transform player = null;
 
     public bool execute_walking;
     public bool execute_sitting;
@@ -30,15 +31,15 @@ public class Test_script : MonoBehaviour
 
 
   
-
+// 캐릭터의 운동상태 나타냄
     public bool walk;
     public bool run;
     public bool sit;
     public bool steal;
     public bool pick_up;
-
-   public bool destermine_new_aim;
-
+    public bool playerTouched;
+//새로운 목표물 유무 판단
+    public bool destermine_new_aim;
 
 
     void Start()
@@ -61,6 +62,8 @@ public class Test_script : MonoBehaviour
         GameObject[] SittingpointsFind = GameObject.FindGameObjectsWithTag("sittingpoint");
         GameObject[] stealingpointsFind = GameObject.FindGameObjectsWithTag("stealingpoint");
         GameObject[] pick_up_pointsFind = GameObject.FindGameObjectsWithTag("pickuppoint");
+        
+
 
         foreach(GameObject g in waypointsFind)
         {
@@ -82,13 +85,17 @@ public class Test_script : MonoBehaviour
 
     }
 
-    bool in_sitting;
+// 해당 상태에 있음을 알려주는 지표
+    bool in_sitting; // 앉음 상태에 있음
     bool in_stealing;
     bool in_pickup;
+    bool in_stop; // 멈춤 상태에 있음
 
+// 코루틴 변수
     Coroutine sitting_start;
     Coroutine stealing_start;
     Coroutine pickup_start;
+    Coroutine stop_start;
 
     public GameObject crowbar;
 
@@ -164,17 +171,42 @@ public class Test_script : MonoBehaviour
         StopCoroutine(pickup_start);
     }
 
+    IEnumerator stop_execute()
+    {
+        yield return new WaitForSeconds(0);
+
+        agent.speed = 0;
+        yield return new WaitForSeconds(10); //10초 동안 멈춰놓음
+
+        playerTouched = false;        
+        destermine_new_aim = false;
+        StopCoroutine(pickup_start);
+    }
+
     public bool ready;
 
     void Update()
     {
 
-        if(!ready)
+        if(!ready || player ==null )
         {
             return;
         }
 
      
+        if (Vector3.Distance(transform.position,player.position) < 0.25f) // 플레이어와 닿는 경우
+        {
+            Debug.Log("Touched"); // 플레이어와 터치되었다고 로그에 띄워줌
+            playerTouched = true; // 플레이어와 터치되었다고 표지 켜줌
+        }
+
+        if(playerTouched) // 만약 플레이어가 터치했다면
+        {
+            agent.speed = 0;
+            in_stop = true;
+            stop_start = StartCoroutine(stop_execute()); // 멈춤 작업 실행
+        }
+        
         if(!destermine_new_aim)
         {
             int what_to_choose = UnityEngine.Random.Range(0, 5);
@@ -233,33 +265,33 @@ public class Test_script : MonoBehaviour
         }
         if (destermine_new_aim)
         {
-            if (walk)
+            if (walk) // 걷기 표지에 해당한다면
             {
 
-                if (Vector3.Distance(transform.position,aim_point.transform.position) > 0.25f)
+                if (Vector3.Distance(transform.position,aim_point.transform.position) > 0.25f) // aimpoint와 거리가 0.25 초과일 때 = 포인트에 다다기 전일 때
                 {
                    
-                    agent.speed = walk_speed;
-                    agent.SetDestination(aim_point.transform.position);
-                    ani.SetInteger("arms", 1);
+                    agent.speed = walk_speed; // 해당 human의 speed 는 걷기 속도
+                    agent.SetDestination(aim_point.transform.position); // aimpoint로 human을 움직임
+                    ani.SetInteger("arms", 1); //애니메이션 설정
                     ani.SetInteger("legs", 1);
                 }
 
-                if (Vector3.Distance(transform.position, aim_point.transform.position) < 0.25f)
+                if (Vector3.Distance(transform.position, aim_point.transform.position) < 0.25f) // aimpoint와 거리가 0.25 미만일 때 = 포인트에 다다를때
                 {
-                    agent.speed = 0;
+                    agent.speed = 0; // 멈춤
 
                     ani.SetInteger("arms", 5);
                     ani.SetInteger("legs", 5);
 
-                    destermine_new_aim = false;
+                    destermine_new_aim = false; // 다시 aim 정함
                 }
 
             }
             if(run)
             {
 
-                if (Vector3.Distance(transform.position, aim_point.transform.position) > 0.25f)
+                if (Vector3.Distance(transform.position, aim_point.transform.position) > 0.25f) 
                 {
                     Debug.Log("going to run");
                     agent.speed = run_speed;
@@ -282,25 +314,25 @@ public class Test_script : MonoBehaviour
             if(sit && !in_sitting)
             {
 
-                if (Vector3.Distance(transform.position, aim_point.transform.position) > 0.25f)
+                if (Vector3.Distance(transform.position, aim_point.transform.position) > 0.25f) // aimpoint와 거리가 0.25 초과일 때 = 포인트에 다다기 전일 때
                 {
                     
-                    agent.speed = walk_speed;
-                    agent.SetDestination(aim_point.transform.position);
+                    agent.speed = walk_speed; 
+                    agent.SetDestination(aim_point.transform.position); // 해당 포인트 쪽으로 걸어오게 함
                     ani.SetInteger("arms", 1);
                     ani.SetInteger("legs", 1);
                 }
 
                 if (Vector3.Distance(transform.position, aim_point.transform.position) < 0.25f)
                 {
-                    agent.speed = 0;
+                    agent.speed = 0; //멈춤
 
 
                     if(!in_sitting)
                     {
                         in_sitting = true;
 
-                        sitting_start = StartCoroutine(sitting_down());
+                        sitting_start = StartCoroutine(sitting_down()); // 앉는 동작 실행
                     }
 
                 }
@@ -362,7 +394,7 @@ public class Test_script : MonoBehaviour
                 }
             }
 
-
+        
         }
 
 
