b8a5c0300e76cf1c7e345f9bf3028401d3f646d0
hongminjeong <meanj@hongminjeongui-MacBookAir.local>
2023-06-12 10:42:19 +0900

    target hidden

--- VRProject/Assets/KSY_Scene/GameManager.cs
+++ VRProject/Assets/KSY_Scene/GameManager.cs
@@ -28,16 +28,20 @@ public class GameManager : MonoBehaviour
     public TMP_Text stageTxt;
     public TMP_Text playTimeTxt;
     public GameObject targetBtn1;
-    public GameObject targetBtn2;
-    public GameObject targetBtn3;
+    // public GameObject targetBtn2;
+    // public GameObject targetBtn3;
     public Image targetImg1;
-    public Image targetImg2;
-    public Image targetImg3;
+    // public Image targetImg2;
+    // public Image targetImg3;
+    
+    public GameObject answer_target1;
+    public GameObject answer_target2;
+    public GameObject answer_target3;
     
     
     private bool toggle1 =false;
-    private bool toggle2 =false;
-    private bool toggle3 =false;
+    // private bool toggle2 =false;
+    // private bool toggle3 =false;
     private bool toggle4=false;
 
 
@@ -59,11 +63,11 @@ public class GameManager : MonoBehaviour
         failPanel.SetActive(false);
         inventoryPanel.SetActive(false);
         targetImg1.gameObject.SetActive(false);
-        targetImg2.gameObject.SetActive(false);
-        targetImg3.gameObject.SetActive(false);
+        // targetImg2.gameObject.SetActive(false);
+        // targetImg3.gameObject.SetActive(false);
         toggle1=false;
-        toggle2=false;
-        toggle3=false;
+        // toggle2=false;
+        // toggle3=false;
         toggle4=false;
         isPlaying=true;
 
@@ -73,29 +77,55 @@ public class GameManager : MonoBehaviour
         stageTxt.text="STAGE "+stage;
         
         //1: 한명 5분, 2: 한명 3분, 3: 두명 5분, 4: 두명 3분, 5: 세명 3분
-        int targetNumber;
+        int targetNumber = 1;
         
         if(stage<=2){
-            targetNumber=1;
             if(stage==1){ // stage 1
-                maxPlayTime=300;
+                maxPlayTime=500;
+                targetImg1.sprite = images[0];
+                //뒤에 안보이게 타겟 생성해놓음
+                answer_target1.SetActive(true);
+                answer_target2.SetActive(false);
+                answer_target3.SetActive(false);
+                
             }
             else{ // stage 2
-                maxPlayTime=180;
+                maxPlayTime=400;
+                targetImg1.sprite = images[1];
+
+                answer_target1.SetActive(false);
+                answer_target2.SetActive(true);
+                answer_target3.SetActive(false);
             }
         }
         else if(stage<=4){
-            targetNumber=2;
             if(stage==3){ // stage 3
                 maxPlayTime=300;
+                targetImg1.sprite = images[2];
+
+
+                answer_target1.SetActive(false);
+                answer_target2.SetActive(false);
+                answer_target3.SetActive(true);
             }
             else{ // stage 4
-                maxPlayTime=180;
+                maxPlayTime=200;
+                targetImg1.sprite = images[0];
+
+
+                answer_target1.SetActive(true);
+                answer_target2.SetActive(false);
+                answer_target3.SetActive(false);
             }
         }
         else{ // stage 5
-            targetNumber=3;
             maxPlayTime=180;
+            targetImg1.sprite = images[1];
+
+
+                answer_target1.SetActive(false);
+                answer_target2.SetActive(true);
+                answer_target3.SetActive(false);
         }
 
         
@@ -107,31 +137,31 @@ public class GameManager : MonoBehaviour
         //     }
         // }
 
-        if(targetNumber==1){
-            targetImg1.sprite = images[0];
+        // if(targetNumber==1){
+        //     targetImg1.sprite = images[0];
 
-            targetBtn1.SetActive(true);
-            targetBtn2.SetActive(false);
-            targetBtn3.SetActive(false);
-        }
-        else if(targetNumber==2){
-            targetImg1.sprite = images[0];
-            targetImg2.sprite = images[1];
+        //     targetBtn1.SetActive(true);
+        //     // targetBtn2.SetActive(false);
+        //     // targetBtn3.SetActive(false);
+        // }
+        // else if(targetNumber==2){
+        //     targetImg1.sprite = images[0];
+        //     targetImg2.sprite = images[1];
 
-            targetBtn1.SetActive(true);
-            targetBtn2.SetActive(true);
-            targetBtn3.SetActive(false);
-        }
-        else{
-            // mj
-            targetImg1.sprite = images[0];
-            targetImg2.sprite = images[1];
-            targetImg3.sprite = images[2];
+        //     targetBtn1.SetActive(true);
+        //     targetBtn2.SetActive(true);
+        //     targetBtn3.SetActive(false);
+        // }
+        // else{
+        //     // mj
+        //     targetImg1.sprite = images[0];
+        //     targetImg2.sprite = images[1];
+        //     targetImg3.sprite = images[2];
 
-            targetBtn1.SetActive(true);
-            targetBtn2.SetActive(true);
-            targetBtn3.SetActive(true);
-        }
+        //     targetBtn1.SetActive(true);
+        //     targetBtn2.SetActive(true);
+        //     targetBtn3.SetActive(true);
+        // }
         
     }
     
@@ -142,11 +172,11 @@ public class GameManager : MonoBehaviour
         failPanel.SetActive(false);
         inventoryPanel.SetActive(false);
         targetImg1.gameObject.SetActive(false);
-        targetImg2.gameObject.SetActive(false);
-        targetImg3.gameObject.SetActive(false);
+        // targetImg2.gameObject.SetActive(false);
+        // targetImg3.gameObject.SetActive(false);
         toggle1 =false;
-        toggle2 =false;
-        toggle3 =false;
+        // toggle2 =false;
+        // toggle3 =false;
         toggle4=false;
 
     }
@@ -194,10 +224,10 @@ public class GameManager : MonoBehaviour
     }
     public void ShowTarget1(){
         if(!toggle1){
-            toggle2=false;
-            toggle3=false;
-            targetImg2.gameObject.SetActive(false);
-            targetImg3.gameObject.SetActive(false);
+            // toggle2=false;
+            // toggle3=false;
+            // targetImg2.gameObject.SetActive(false);
+            // targetImg3.gameObject.SetActive(false);
             
             targetImg1.gameObject.SetActive(true);
             toggle1=true;
@@ -207,35 +237,35 @@ public class GameManager : MonoBehaviour
             toggle1=false;
         }
     }
-    public void ShowTarget2(){
-        if(!toggle2){
-            toggle1=false;
-            toggle3=false;
-            targetImg1.gameObject.SetActive(false);
-            targetImg3.gameObject.SetActive(false);
+    // public void ShowTarget2(){
+    //     if(!toggle2){
+    //         toggle1=false;
+    //         toggle3=false;
+    //         targetImg1.gameObject.SetActive(false);
+    //         targetImg3.gameObject.SetActive(false);
             
-            targetImg2.gameObject.SetActive(true);
-            toggle2=true;
-        }
-        else{
-            targetImg2.gameObject.SetActive(false);
-            toggle2=false;
-        }
-    }
-    public void ShowTarget3(){
-        if(!toggle3){
-            toggle1=false;
-            toggle2=false;
-            targetImg1.gameObject.SetActive(false);
-            targetImg2.gameObject.SetActive(false);
-            targetImg3.gameObject.SetActive(true);
-            toggle3=true;
-        }
-        else{
-            targetImg3.gameObject.SetActive(false);
-            toggle3=false;
-        }
-    }
+    //         targetImg2.gameObject.SetActive(true);
+    //         toggle2=true;
+    //     }
+    //     else{
+    //         targetImg2.gameObject.SetActive(false);
+    //         toggle2=false;
+    //     }
+    // }
+    // public void ShowTarget3(){
+    //     if(!toggle3){
+    //         toggle1=false;
+    //         toggle2=false;
+    //         targetImg1.gameObject.SetActive(false);
+    //         targetImg2.gameObject.SetActive(false);
+    //         targetImg3.gameObject.SetActive(true);
+    //         toggle3=true;
+    //     }
+    //     else{
+    //         targetImg3.gameObject.SetActive(false);
+    //         toggle3=false;
+    //     }
+    // }
     public void showInventory(){
         if(!toggle4){
             inventoryPanel.SetActive(true);
